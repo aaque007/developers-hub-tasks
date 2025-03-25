afbf830d9c37a0c69a22c8e8f9b0f054eb76e3e4
aaque007 <157409845+aaque007@users.noreply.github.com>
2025-03-26 01:34:24 +0500

    Create task_model.dart

---
+++ task_model.dart
@@ -0,0 +1,27 @@
+class Task {
+  final String id;
+  final String title;
+  bool isCompleted;
+
+  Task({
+    required this.id,
+    required this.title,
+    this.isCompleted = false,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'isCompleted': isCompleted,
+    };
+  }
+
+  factory Task.fromMap(Map<String, dynamic> map) {
+    return Task(
+      id: map['id'] ?? '',
+      title: map['title'] ?? '',
+      isCompleted: map['isCompleted'] ?? false,
+    );
+  }
+}
